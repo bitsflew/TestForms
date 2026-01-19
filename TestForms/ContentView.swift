b4df00ce2a58829d13613f2b194830b1d66b2285
Henk Kampman <henk.kampman@secondmove.com>
2026-01-19 09:54:39 +0100

    Initial Commit

---
+++ TestForms/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestForms
+//
+//  Created by Henk on 19/01/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestForms/TestFormsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestFormsApp.swift
+//  TestForms
+//
+//  Created by Henk on 19/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct TestFormsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
